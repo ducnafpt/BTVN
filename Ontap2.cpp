955dc8d133c4095d6db53c2986cd8e2719cc6afc
ducnafpt <cuncan37@gmail.com>
2019-12-13 15:33:50 +0700

    Ontap

---
+++ Ontap2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	for(int i = 0; i < 4; i++)
+	{
+		printf("|");
+		for(int j = 0; j < 6; j++)
+		{
+			printf("+");
+		}
+		printf("|\n");
+	}
+}
+
---
+++ Ontap5.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("Nhap n: \n");
+	scanf("%d", &n);
+	if(n <= 2){
+		printf("Khong tim duoc \n");
+	}else{
+		for(int i = 2; i < n; i++)
+		{
+			int count = 0;
+			for(int j = 1; j <= i; j++)
+			{
+				if(i % j == 0){
+					count++;
+				}
+			}
+			if(count <= 2){
+				printf("%d la so nguyen to \n", i);
+			}
+		}
+	}
+	return 0;
+}
