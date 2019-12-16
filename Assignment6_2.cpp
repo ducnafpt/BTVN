f3371b0297baa436bb1b21fdaa17250fa4903da3
ducnafpt <58342590+ducnafpt@users.noreply.github.com>
2019-12-16 17:51:27 +0700

    Update Assignment6_2.cpp

--- Assignment6_2.cpp
+++ Assignment6_2.cpp
@@ -4,7 +4,7 @@ int main(){
 	printf("Nhap n: \n", &n);
 	scanf("%d", &n);
 	int ary[n];
-	for(int i = 0; i <= n; i++)
+	for(int i = 0; i < n; i++)
 	{
 		printf("Nhap ary[%d]: \n", i);
 		scanf("%d", &ary[i]);
@@ -12,7 +12,7 @@ int main(){
 	int tong = 0;
 	int count = 0;
 	float tbc;
-	for(int i = 1; i <= n; i+=2)
+	for(int i = 1; i < n; i+=2)
 	{
 		if(ary[i] % 2 != 0){
 			tong += ary[i];
