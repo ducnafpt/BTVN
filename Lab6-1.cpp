daff7e7a32a1dcfbd8fc30274b952ccb3e8f9492
ducnafpt <58342590+ducnafpt@users.noreply.github.com>
2019-12-23 11:05:04 +0700

    Update Lab6-1.cpp

--- Lab6-1.cpp
+++ Lab6-1.cpp
@@ -1,37 +1,21 @@
 #include <stdio.h>
-
 int main(){
-
 	int n;
-
-	printf("Nhap n=\n");
-
-	scanf("%d",&n);
-
+	printf("Nhap n la: \n");
+	scanf("%d", &n);
 	int ary[n];
 
-	for (int i = 0; i < n; ++i)
-
+	for (int i = 0; i < n; i++)
 	{
-
-		printf("Nhap ary[%d]\n",i);
-
-		scanf("%d",&ary[i]);
-
+		printf("Nhap ary[%d]: \n", i);
+		scanf("%d", &ary[i]);
 	}
 
-	for (int i = 0; i < n; ++i)
-
+	for (int i = 0; i < n; i++)
 	{
-
-		if(ary[i]%2!=0){
-
-			printf("%d\n",ary[i]);
-
+		if(ary[i] % 2 != 0){
+			printf("%d \n", ary[i]);
 		}
-
 	}
-
 	return 0;
-
 }
